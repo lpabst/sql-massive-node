946a1b34a67444504cda64986d0d852f902220e2
loren pabst <loren pabst>
2017-05-17 15:07:44 -0600

    updated code in controller and index

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const massive = require('massive');
-const controller = require('./controller');
+const controller = require('./productsCtrl.js');
+const session = require('express-session');
 
-var db = massive.connectSync({
+const app = module.exports = express();
+
+var massiveInstance = massive.connectSync({
     connectionString: 'postgres://sitedvmuehvxqj:6e5588b9f2489c1f12b652893d9648022b49aa88664bd01d03c0bebea49e8a85@ec2-23-23-227-188.compute-1.amazonaws.com:5432/db9kauonju9ri1?ssl=true'
 });
 
-var app = module.exports = express();
+app.set('db', massiveInstance);
+var db = app.get('db');
 
 app.use(bodyParser.json());
 
 
+app.get('/api/products', controller.getAllProducts);
+app.get('/api/products/:id', controller.getProductById);
+
+app.post('/api/products', controller.createProduct);
+
+app.put('/api/products/:id', controller.updateProduct);
+
+app.delete('/api/products/:id', controller.deleteProduct);
 
 
 
@@ -19,4 +31,4 @@ app.use(bodyParser.json());
 
 
 
-app.listen(8000, console.log('hi boyyy'))
+app.listen(8000, console.log('yo 8000'))
---
+++ productsCtrl.js
@@ -0,0 +1,48 @@
+var app = require('./index.js');
+var db = app.get('db');
+console.log(db);
+
+module.exports = {
+
+    getAllProducts: function(req, res, next){
+        db.getAllProducts(function(err, products){
+            return res.send(products);
+        })
+    },
+
+    getProductById: function(req, res, next){
+        let id = req.params.id;
+
+        db.getProductById([id], function(err, products){
+            return res.send(products);
+        })
+    },
+
+    createProduct: function(req, res, next){
+        let id = req.body.id;
+        let name = req.body.name;
+        let desc = req.body.description;
+        let price = req.body.price;
+        let imageurl = req.body.imageurl;
+
+        db.createProduct([id, name, desc, price, imageurl], function(err, products){
+            return res.send('ok');
+        })
+    },
+
+    updateProduct: function(req, res, next){
+        db.updateProduct(function(err, products){
+            return res.send(products);
+        })
+    },
+
+    deleteProduct: function(req, res, next){
+        db.deleteProduct(function(err, products){
+            return res.send(products);
+        })
+    }
+
+}
+
+
+
