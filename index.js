f8baf09ef2bcfae8a04897b06ea20085c8b58840
loren pabst <loren pabst>
2017-05-17 13:35:07 -0600

    created db, set up app server basic code

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+
+var db = massive.connectSync({
+    connectionString: 'postgres://sitedvmuehvxqj:6e5588b9f2489c1f12b652893d9648022b49aa88664bd01d03c0bebea49e8a85@ec2-23-23-227-188.compute-1.amazonaws.com:5432/db9kauonju9ri1?ssl=true'
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+
+
+app.listen(8000, console.log('hi boyyy'))
